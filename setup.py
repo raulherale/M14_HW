ff69115f075b90aa42c3bad31f5b473984bafa8e
raulherale <51846188+raulherale@users.noreply.github.com>
2022-07-11 17:11:53 -0400

    Setup File

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+
+from setuptools import setup , find_packages
+
+setup(name='BookLover',
+      version='1.1',
+      description='BookLover class',
+      url='https://github.com/raulherale/M14Classwork.git',
+      author='Ashrith',
+      author_email='svz5ya@virginia.edu',
+      license='MIT',
+      packages=find_packages()
+     )
